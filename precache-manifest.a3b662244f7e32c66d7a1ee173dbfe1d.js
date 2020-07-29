b91cadd4b7c2b41a109132d53747a7324e1186f9
Minty <kinostl@gmail.com>
2020-07-28 23:37:09 -0400

    Updates

---
+++ precache-manifest.a3b662244f7e32c66d7a1ee173dbfe1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2530782e859d2568bd8e2131fd8f79f5",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "f734bb62f9694d6c30f4",
+    "url": "/rolodex/static/css/2.618869c7.chunk.css"
+  },
+  {
+    "revision": "4afc8779f4805f0d9547",
+    "url": "/rolodex/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f734bb62f9694d6c30f4",
+    "url": "/rolodex/static/js/2.c100d101.chunk.js"
+  },
+  {
+    "revision": "697fae871f3597e1930c1e3f5a974780",
+    "url": "/rolodex/static/js/2.c100d101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4afc8779f4805f0d9547",
+    "url": "/rolodex/static/js/main.e238fa73.chunk.js"
+  },
+  {
+    "revision": "604800cde4d3fb0ab0cb",
+    "url": "/rolodex/static/js/runtime-main.b7003d66.js"
+  }
+]);
